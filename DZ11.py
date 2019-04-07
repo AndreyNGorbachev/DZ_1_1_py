664ea027609ae9b86f6156928a8d22595684b132
Andrey Gorbachev <andrey-ng@yandex.ru>
2019-04-07 17:16:57 +0300

    Передаём параметры в методы

--- DZ11.py
+++ DZ11.py
@@ -14,7 +14,7 @@ class DZ11(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="group_DZ_1_1", header="DZ_1_1", footer="DZ_1_1")
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -24,19 +24,19 @@ class DZ11(unittest.TestCase):
     def return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group_DZ_1_1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("DZ_1_1")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("DZ_1_1")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
